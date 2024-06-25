71efe535e88b0b45719cd8cce188b972206f1371
summyalena <summyalena@gmail.com>
2024-06-25 10:21:48 +0100

    Add animation to reusable component

--- app/components/ui/Container.js
+++ app/components/ui/Container.js
@@ -2,17 +2,28 @@
 
 import styles from './ui.module.css';
 import useIntersectionView from '../hooks/useIntersectionView';
+import { useEffect, useState } from 'react';
 
-function Container({children, className}) {
-  const [ref, isVisible] = useIntersectionView({
-    threshold: 0.1
-  });
+function Container({ children, className }) {
+  const [ref, isVisible] = useIntersectionView({ threshold: 0.1 });
+  const [triggerAnimation, setTriggerAnimation] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      setTriggerAnimation(true);
+    } else {
+      setTriggerAnimation(false);
+    }
+  }, [isVisible]);
 
   return (
-    <section ref={ref} className={`full-width ${className} ${styles.sectionContainer} ${isVisible ? styles.visible : ''}`}>
+    <section
+      ref={ref}
+      className={`full-width ${className} ${styles.sectionContainer} ${triggerAnimation ? styles.visible : ''}`}
+    >
       {children}
     </section>
-  )
+  );
 }
 
-export default Container
+export default Container;
